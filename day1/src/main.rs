647faef6125ec274da0f1ae3144ce31493fb4506
neoscript <neoscripting@gmail.com>
2025-12-04 20:43:51 +0100

    feat: solved day1 part 2
    utilized some nice unit tests to validate logic of smaller pieces so we can build it correctly

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -9,6 +9,9 @@ fn main() {
 
     let result_one = part_one();
     println!("result from part one: {}", result_one);
+
+    let result_two = part_two();
+    println!("result from part two: {}", result_two);
 }
 
 fn input_one(path: &Path) -> Vec<RotationInstruction> {
@@ -27,11 +30,25 @@ fn part_one() -> u64 {
 
     let mut zero_counts = 0;
     for i in instructions {
-        println!("{:?} -> apply({:?})", dial, i);
+        // println!("{:?} -> apply({:?})", dial, i);
         dial.turn(&i);
         if dial.at_zero() {
             zero_counts += 1
         }
+    }
+    zero_counts
+}
+
+fn part_two() -> u64 {
+    let path = Path::new("input1.txt");
+    let mut dial = Dial::default();
+    let instructions = input_one(path);
+
+    let mut zero_counts = 0;
+    for i in instructions {
+        // println!("{:?} -> apply({:?})", dial, i);
+        let counted = dial.turn_and_count_zeros(&i);
+        zero_counts += counted;
     }
     zero_counts
 }
--- day1/src/models.rs
+++ day1/src/models.rs
@@ -24,12 +24,25 @@ impl Dial {
         }
     }
 
+    pub fn turn_and_count_zeros(&mut self, rotation: &RotationInstruction) -> u64 {
+        let mut counter = 0;
+        for _step in 0..rotation.quantity {
+            self.tick(&rotation.direction);
+
+            if self.at_zero() {
+                counter += 1;
+            }
+        }
+
+        counter
+    }
+
     pub fn at_zero(self) -> bool {
         self.0 == 0
     }
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Direction {
     Left = -1,
     Right = 1,
@@ -45,7 +58,7 @@ impl TryFrom<&str> for RotationInstruction {
     type Error = String;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        println!("trying to convert: {}", value);
+        // println!("trying to convert: {}", value);
         let (dir_indicator, quantity_str) =
             value.split_at_checked(1).ok_or("Could not split row")?;
 
@@ -65,5 +78,66 @@ impl TryFrom<&str> for RotationInstruction {
             direction,
             quantity,
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_instruction_parsing() {
+        let instruction = "R1000";
+        let result: RotationInstruction = instruction.try_into().expect("should parse");
+
+        assert_eq!(result.direction, Direction::Right);
+        assert_eq!(result.quantity, 1000);
+    }
+
+    #[test]
+    fn test_count_zeros_per_click() {
+        let mut dial = Dial::default();
+        let test_rotation = RotationInstruction {
+            direction: Direction::Right,
+            quantity: 1000,
+        };
+        let result = dial.turn_and_count_zeros(&test_rotation);
+
+        assert_eq!(
+            result, 10,
+            "applying R1000 to Dial starting at 50 should result in 10 for the zero counts"
+        );
+
+        assert_eq!(dial.0, 50);
+    }
+
+    #[test]
+    fn test_count_zeros_with_two_times_r500() {
+        let mut dial = Dial::default();
+        let first_rotation = "R500"
+            .try_into()
+            .expect("should create right 500 instruction");
+
+        let second_rotation = "R500"
+            .try_into()
+            .expect("should create right 500 instruction");
+
+        let mut counter = 0;
+        counter += dial.turn_and_count_zeros(&first_rotation);
+        counter += dial.turn_and_count_zeros(&second_rotation);
+
+        assert_eq!(counter, 10);
+        assert_eq!(dial.0, 50);
+    }
+
+    #[test]
+    fn test_simple_turn_and_count() {
+        let mut dial = Dial::default();
+        let rotation = "R50".try_into().expect("should make rotation instruction");
+        let mut counter = 0;
+        counter += dial.turn_and_count_zeros(&rotation);
+
+        assert_eq!(counter, 1);
+        assert_eq!(dial.0, 0);
     }
 }
